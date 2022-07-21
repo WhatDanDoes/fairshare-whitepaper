ab539a3f7f7811fa549bc422d0ac5aca1b1f8133
Daniel Bidulock <daniel@capitolhill.ca>
2022-07-21 10:00:35 -0600

    Hello, favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/css');
   eleventyConfig.addPassthroughCopy('src/resources');
+  eleventyConfig.addPassthroughCopy('src/favicon');
   return {
     dir: {
       input: 'src',
