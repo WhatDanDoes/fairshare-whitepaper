d7ada89eb2d9e991e283ad91773974350fcc1c62
Daniel Bidulock <daniel@capitolhill.ca>
2022-07-21 09:38:48 -0600

    Copy static resource directory to site build

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addPassthroughCopy('src/resources');
   return {
     dir: {
       input: 'src',
